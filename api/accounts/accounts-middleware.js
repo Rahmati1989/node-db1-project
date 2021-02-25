00a687ea737465a3a0401712da26de63ffa670ec
Rafiullah Rahmati <rafiullahrahmati@Rafiullahs-MacBook-Pro.local>
2021-02-24 22:51:09 -0800

    Finished account-model and accounts-router

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,24 @@
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
   // DO YOUR MAGIC
+  const checkName = req.accounts.map(account => account.name == req.body.name)
+  if(checkName) {
+    res.status(400).json({error: "That name is already taken"})
+  }
 }
 
 exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
+   const account = req.accounts.map(account => account.id == req.params.id)
+  if(!account) {
+    res.status(400).json({error: "account not found"})
+  } else {
+    req.accountInfo === account
+    next()
+  }
+
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,23 +1,77 @@
 const router = require('express').Router()
+const db = require('../../data/db-config')
+// const dbModel = require("./accounts-model")
+const { limit, del } = require('../../data/db-config')
 
 router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const accounts = await db.select("*").from("accounts")
+    res.json(accounts)
+  }catch(err) {
+    next(err)
+  }
+
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+const budgets = await db.select("*")
+.from("accounts")
+.where("id", req.params.id)
+res.json(budgets)
+limit(1)
+  }catch (err) {
+    next(err)
+  }
+
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const [id] = await db.insert({
+      name: req.body.name,
+      budget: req.body.budget
+    }).into("accounts")
+    const budgets = await db("accounts")
+    .where("id",id)
+    .first()
+    res.status(201).json(budgets)
+    
+  }catch (err) {
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', async(req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    await db("accounts")
+    .update({
+      name: req.body.name,
+      budget: req.body.budget
+    })
+    .where("id", req.params.id)
+    const budgets = await db("accounts")
+    .where("id",id)
+    .first()
+    res.json(budgets)
+  }catch(err) {
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
   // DO YOUR MAGIC
+try {
+await db("accounts")
+.where("id",req.params.id)
+del()
+}catch (err) {
+  next(err)
+}
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
@@ -26,6 +80,7 @@ router.use((err, req, res, next) => { // eslint-disable-line
     message: 'something went wrong inside the accounts router',
     errMessage: err.message,
   })
+  
 })
 
 module.exports = router;
